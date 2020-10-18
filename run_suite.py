3b5408bfceca1c82283522da660f4d9f1d45760c
小H <290911574@qq.com>
2020-10-18 15:53:56 +0800

    添加测试套件中的测试脚本

--- run_suite.py
+++ run_suite.py
@@ -1,5 +1,6 @@
 import unittest,time
-from script.login import login
+#from script.login import login
+from script.login_param import login
 from script.approve import approve
 from script.trust import trust
 from script.tender import tender
@@ -10,6 +11,10 @@ from lib.HTMLTestRunner_PY3 import HTMLTestRunner
 #将测试脚本添加到测试套件中
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(login))
+suite.addTest(unittest.makeSuite(approve))
+suite.addTest(unittest.makeSuite(trust))
+suite.addTest(unittest.makeSuite(tender))
+suite.addTest(unittest.makeSuite(test_tender_process))
 
 #运行套件并生成测试报告
 report_file = app.BASE_DIR + '/report/report{}.html'.format(time.strftime("%Y%m%d-%H%M%S"))
